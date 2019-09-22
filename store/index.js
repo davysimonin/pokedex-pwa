2784c751a25907d8b66a75cd8318a36f72079642
Davy <davy@addock.co>
2019-09-22 15:24:00 +0200

    added actions, filters and some state mutations

--- store/index.js
+++ store/index.js
@@ -1,7 +1,11 @@
 export const state = () => ({
   pokemons: [],
   filteredPokemons: [],
-  nextUrl: ''
+  nextUrl: '',
+  searchFilter: '',
+  typeFilter: [],
+  physicalFilter: [],
+  isFetching: false
 })
 
 export const mutations = {
@@ -9,11 +13,46 @@ export const mutations = {
     state.pokemons = pokemons
     state.filteredPokemons = pokemons
   },
+  setFilteredPokemons (state, filteredPokemons) {
+    state.filteredPokemons = filteredPokemons
+  },
+  resetFilteredPokemons (state) {
+    state.filteredPokemons = state.pokemons
+  },
   setNextPokemons (state, pokemons) {
     state.pokemons = [...state.pokemons, ...pokemons]
-    state.filteredPokemons = [...state.pokemons]
+    state.filteredPokemons = state.pokemons
   },
   setNextFetchUrl (state, url) {
     state.nextUrl = url
+  },
+  setSearch (state, str) {
+    state.searchFilter = str
+  },
+  setFilter (state, { value, filter }) {
+    state[filter] = value
+  },
+  isFetching (state, bool) {
+    state.isFetching = bool
+  }
+}
+
+export const actions = {
+  async loadNextPokemons ({ state, commit }) {
+    commit('isFetching', true)
+    const response = await this.$axios.get(`${process.env.CUSTOM_POKEAPI}/${state.nextUrl}`)
+    commit('setNextPokemons', response.data.pokemons)
+    commit('setNextFetchUrl', response.data.nextUrl)
+    commit('isFetching', false)
+  },
+  async fetchFilteredList ({ state, commit }) {
+    const { typeFilter, physicalFilter, searchFilter } = state
+    if (!typeFilter.length && !physicalFilter.length && !searchFilter) {
+      commit('resetFilteredPokemons')
+      return
+    }
+    const filters = { types: state.typeFilter, physical: state.physicalFilter, search: searchFilter }
+    const response = await this.$axios.post(`${process.env.CUSTOM_POKEAPI}/api/filterpokemons`, { filters })
+    commit('setFilteredPokemons', response.data)
   }
 }
